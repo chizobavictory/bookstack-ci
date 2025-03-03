73c786db9f2a129a7619d29372d0f9ec5fd5d98d
chizobavictory <chizoba.victory1@gmail.com>
2025-03-03 02:36:27 +0100

    Update Terraform backend configuration for Bookstack EKS deployment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = var.remote_data_s3_bucket
-    key     = "${var.environment}/eks-cluster/terraform.tfstate"
-    region  = var.aws_region
-    dynamodb_table = "${var.environment}-ekscluster"
+    bucket         = "bookstack-eks"
+    key            = "test/eks-cluster/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "bookstack-eks"
     encrypt        = true
   }
 }
